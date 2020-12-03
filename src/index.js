24fa0784689715bdc373def8646ef6c8a7d6825f
Liza <y.chukhareva@zakaz.ua>
2020-12-03 14:10:54 +0200

    Add thunk

--- src/index.js
+++ src/index.js
@@ -4,24 +4,15 @@ import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux'
-import rootReducer from './store/redusers'
-import { createStore, compose , applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+import { createStore, applyMiddleware  } from 'redux';
+import rootReducer from './store/rootReducer';
+import thunk from 'redux'
 
 
-const composeEnhancers =
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ||
-  // window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ trace: true, traceLimit: 25 }) ||
-  compose
-;
-
-const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(thunk))
+const store = createStore(rootReducer,
+  applyMiddleware(thunk)
 );
 
-
-console.log("🚀 ~ file: index.js ~ line 11 ~ store", store)
 
 ReactDOM.render(
   <React.StrictMode>
